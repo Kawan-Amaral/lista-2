6504348c584c7a7f5cc96db0c5d9669d99fafe1a
Kawan-Amaral <kawan.melo@aluno.ifsp.edu.br>
2025-06-04 21:19:28 -0300

    Add files via upload

---
+++ Ex_5/Ex_5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double bas3;
+            double h;
+
+            Console.Write("Digite a altura do seu Retangulo:");
+            h = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite a Base do seu Retangulo:");
+            bas3 = double.Parse(Console.ReadLine());
+
+            double area = bas3 * h;
+
+            if (area >= 100)
+            {
+                Console.WriteLine("Terreno Muito Grande!!");
+            }
+
+            else
+            {
+                Console.WriteLine("A area é {0}", area);
+            }
+        }
+    }
+}
