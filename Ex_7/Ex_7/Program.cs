9460bb15f5d732f4d2a98d16edff04904a8c27ac
Kawan-Amaral <kawan.melo@aluno.ifsp.edu.br>
2025-06-04 21:21:30 -0300

    Add files via upload

---
+++ Ex_7/Ex_7/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o primeiro lado: ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo lado: ");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o terceiro lado: ");
+            double c = double.Parse(Console.ReadLine());
+
+            
+            if (a + b > c && a + c > b && b + c > a)
+            {
+                Console.Write("Forma um triângulo: ");
+
+                if (a == b && b == c)
+                {
+                    Console.WriteLine("Equilátero");
+                }
+                else if (a == b || a == c || b == c)
+                {
+                    Console.WriteLine("Isósceles");
+                }
+                else
+                {
+                    Console.WriteLine("Escaleno");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Os lados não formam um triângulo.");
+            }
+        }
+    }
+}
