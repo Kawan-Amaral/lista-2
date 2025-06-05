8a17d98751508059c9a4667f58a7f5fffd358e51
Kawan-Amaral <kawan.melo@aluno.ifsp.edu.br>
2025-06-04 21:21:58 -0300

    Add files via upload

---
+++ Ex_8/Ex_8/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor de A: ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor de B: ");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor de C: ");
+            double c = double.Parse(Console.ReadLine());
+
+            
+            double[] lados = { a, b, c };
+            Array.Sort(lados); 
+
+            double cat1 = lados[0];
+            double cat2 = lados[1];
+            double hipo = lados[2];
+
+            
+            if (Math.Pow(hipo, 2) == Math.Pow(cat1, 2) + Math.Pow(cat2, 2))
+            {
+                Console.WriteLine("É um triângulo retângulo.");
+            }
+            else
+            {
+                Console.WriteLine("Não é um triângulo retângulo.");
+            }
+        }
+    }
+}
